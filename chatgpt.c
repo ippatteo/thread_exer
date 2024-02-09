d72d4931585a7db7054228c5705477214077fcd7
mcamilli <mcamilli@student.42roma.it>
2024-02-09 10:33:04 +0100

    ok

--- chatgpt.c
+++ chatgpt.c
@@ -1,16 +1,156 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_utils.c                                      :+:      :+:    :+:   */
+/*   chatgpt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kevi il re, <capitano delle troie>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/06 12:12:00 by kevi il re,       #+#    #+#             */
-/*   Updated: 2024/02/09 10:14:35 by kevi il re,      ###   ########.fr       */
+/*   Created: 2023/10/10 17:06:42 by mcamilli          #+#    #+#             */
+/*   Updated: 2024/02/09 10:02:42 by kevi il re,      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include <time.h>
+#include <sys/time.h>
+# include <sys/types.h>
+# include <stdint.h>
+# include <pthread.h>
+# include <stdlib.h>
+
+
+
+typedef struct s_philo
+{
+	int	id_philo;	//numero filosofi
+	int meal;
+	int sated;
+	int dead;
+	int	t_die;	//time to die
+	int	t_eat;	//time to eat
+	int	t_sleep;	//time to sleep
+	int	n_eat;
+	int	end_philo;
+	pthread_mutex_t *fork_l; //how meny time they have to eat
+	pthread_mutex_t *fork_r;
+	struct s_data *data;
+	pthread_mutex_t	lock_dead;
+	
+}	t_philo;
+
+typedef struct s_data
+{
+	int	n_philo;	//numero filosofi
+	int	t_die;	//time to die
+	int sms_flag;
+	int	t_eat;	//time to eat
+	int	t_sleep;	//time to sleep
+	int	n_eat; //how meny time they have to eat
+	int end;
+	int sated_condition;
+	t_philo *philo;
+	pthread_mutex_t *forks;
+	pthread_t *threads;
+	pthread_mutex_t lock;
+	uint64_t	time;
+}	t_data;
+
+void math_sated(t_data *data);
+void		*atac(void *ptr);
+int			ft_atoi(char *str);
+int			error(t_data *data);
+int			ft_isdigit(int c);
+int			ft_usleep(uint64_t time);
+void		init(t_data *data, int ac, char **av);
+void 		init_philos(t_data *data);
+void 		init_fork(t_data *data);
+void 		init_threads(t_data *data);
+void		ft_eat(t_philo *philo, pthread_t delorian);
+void 		ft_thinking(t_philo *philo);
+void 		*ft_routine(void *ptr);
+void		alloc(t_data *data);
+void		sms(t_data *data, char *str, int id);
+uint64_t	get_time(void);
+void	free_all(t_data *data);
+int ft_kill_all(t_data *data, int i);
+
+#endif
+
+
 #include "../philo.h"
+
+int ft_kill_all(t_data *data, int i)
+{
+	if (data->philo[i].dead == 1)
+	{
+		pthread_mutex_lock(&data->lock);
+		data->end = 1;
+		pthread_mutex_unlock(&data->lock);
+		sms(data, "is dead", data->philo[i].id_philo);
+		return (1);
+	}
+	else
+		return (0);
+}
+
+void *atac(void *ptr)
+{
+	t_data	*data;
+	int		i;
+	int a;
+	
+	data = (t_data *)ptr;
+	while (1)
+	{
+		a = 0;
+		i = 0;
+		while (i < data->n_philo)
+		{
+			if(data->philo[i].end_philo == 1)
+			{
+				if (ft_kill_all(data, i))
+					return (NULL);
+				if(data->philo[i].sated == 1)
+				{
+					a = 0;
+					while (a < data->n_philo && data->philo[a].sated == 1)
+						a++;
+					if (a > data->n_philo)
+						return (NULL);
+				}
+			}
+			i++;
+		}
+	}
+}
+
+int	main(int ac, char **av)
+{
+	t_data	data;
+	
+	if (ac >= 5 && ac <= 6)
+	{
+		init(&data, ac, av);
+		if (error(&data))
+			return (0);
+		alloc(&data);
+		init_philos(&data);
+		init_fork(&data);
+		init_threads(&data);
+		free_all(&data);
+	}
+	else
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+}
 
 void	*timer(void *ptr)
 {
@@ -51,7 +191,7 @@ void ft_eat(t_philo *philo, pthread_t delorian)
 	}
 	pthread_join(delorian, NULL);
 	sms(philo->data, "is eating", philo->id_philo);
-	usleep(philo->t_eat);
+	ft_usleep(philo->t_eat);
 	pthread_create(&delorian, NULL, &timer, &philo);
 	 philo->meal++;
 	pthread_mutex_unlock(philo->fork_l);
@@ -68,7 +208,7 @@ void ft_sleeping(t_philo *philo)
 		philo->dead = 1;
 		philo->end_philo = 1;
 		pthread_mutex_lock(&philo->lock_dead);
-		printf(" pipolo\n");
+		printf(" pi;polo\n");
 	}
 	else
 	{
@@ -187,7 +327,6 @@ void free_all(t_data *data)
 		pthread_mutex_destroy(&data->forks[i]);
 		i++;
 	}
-	pthread_mutex_destroy(&data->lock);
 	free(data->philo);
 	free(data->forks);
 	free(data->threads);
@@ -204,4 +343,84 @@ void math_sated(t_data *data)
 		data->sated_condition += data->philo[i].id_philo;
 		i++;
 	}
+}
+
+#include "../philo.h"
+
+
+void	init(t_data *data, int ac, char **av)
+{
+	data->n_philo = ft_atoi(av[1]);
+	data->t_die = ft_atoi(av[2]);
+	data->t_eat = ft_atoi(av[3]);
+	data->t_sleep = ft_atoi(av[4]);
+	data->time = get_time();
+	data->sms_flag = 0;
+	if (ac == 6)
+		data->n_eat = ft_atoi(av[5]);
+	else
+		data->n_eat = 0;
+}
+
+void init_philos(t_data *data)
+{
+	int i;
+	
+	i = 0;
+	while (i < data->n_philo)
+	{
+		data->philo[i].id_philo = i + 1;
+		data->philo[i].t_die = data->t_die;
+		data->philo[i].t_eat = data->t_eat;
+		data->philo[i].t_sleep = data->t_sleep;
+		data->philo[i].n_eat = data->n_eat;
+		data->philo[i].meal = 0;
+		data->philo[i].sated = 0;
+		data->philo[i].dead = 0;
+		data->philo[i].end_philo = 0;
+		data->philo[i].data = data;
+		
+		i++;
+	}
+}
+
+void init_fork(t_data *data)
+{
+	int	i;
+	
+	i = 0;
+	while(i++ < data->n_philo)
+		pthread_mutex_init(&data->forks[i], NULL);
+	data->philo[0].fork_l = &data->forks[0];
+	data->philo[0].fork_r = &data->forks[data->n_philo - 1];
+	i = 1;
+	while (i < data->n_philo)
+	{
+		data->philo[i].fork_l = &data->forks[i];
+		data->philo[i].fork_r = &data->forks[i - 1];
+		i++;
+	}
+}
+
+void init_threads(t_data *data)
+{
+	pthread_t pula;
+	int i;
+	
+	data->end = 0;
+	i = 0;
+	pthread_mutex_init(&data->lock, NULL);
+	pthread_create(&pula, NULL, &atac, &data);
+	while (i < data->n_philo)
+	{
+		pthread_create(&data->threads[i], NULL, &ft_routine, &data->philo[i]);
+		i++;
+		usleep(1000);//qui ritardi i filosofi
+	}
+	while(i < data->n_philo)
+	{
+		pthread_join(data->threads[i], NULL);
+		i++;
+	}
+	pthread_join(pula, NULL);
 }
--- philosophers/Philosophers.c
+++ philosophers/Philosophers.c
@@ -6,7 +6,7 @@
 /*   By: kevi il re, <capitano delle troie>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 17:40:26 by kevi il re,       #+#    #+#             */
-/*   Updated: 2024/02/09 04:50:19 by kevi il re,      ###   ########.fr       */
+/*   Updated: 2024/02/09 10:26:01 by kevi il re,      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int ft_kill_all(t_data *data, int i)
 		pthread_mutex_lock(&data->lock);
 		data->end = 1;
 		pthread_mutex_unlock(&data->lock);
-		pthread_mutex_destroy(&data->lock);
 		sms(data, "is dead", data->philo[i].id_philo);
 		return (1);
 	}
@@ -31,6 +30,7 @@ void *atac(void *ptr)
 {
 	t_data	*data;
 	int		i;
+	int 	a;
 	
 	data = (t_data *)ptr;
 	while (1)
@@ -44,15 +44,17 @@ void *atac(void *ptr)
 					return (NULL);
 				if(data->philo[i].sated == 1)
 				{
-					pthread_mutex_destroy(&data->lock);
-					data->sated_condition -= data->philo[i].id_philo;
-					if (!data->sated_condition)
+					a = 0;
+					while (a < data->n_philo && data->philo[a].sated == 1)
+						a++;
+					if (a > data->n_philo)
 						return (NULL);
 				}
 			}
 			i++;
 		}
 	}
+	return (NULL);
 }
 
 int	main(int ac, char **av)
